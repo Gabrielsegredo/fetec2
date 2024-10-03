18f00b5353493fa6bc630bfb07df7d94ca00a699
Gabrielsegredo <134285397+Gabrielsegredo@users.noreply.github.com>
2024-10-02 21:11:24 -0300

    SUBINDO

---
+++ script.js
@@ -0,0 +1,62 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
+
+
+
+
+// Mostrar o botão ao rolar a página
+window.addEventListener('scroll', function() {
+  const btn = document.getElementById('back-to-top-btn');
+  
+  if (window.scrollY > 300) { // Aparece após rolar 300px
+      btn.style.display = 'block';
+  } else {
+      btn.style.display = 'none';
+  }
+});
